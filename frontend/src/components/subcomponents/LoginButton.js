68fed675de1f9344c1fdcf4c224ab8ff1e0481df
pocofrosty <91432961+pocofrosty@users.noreply.github.com>
2022-04-20 15:05:04 -0400

    Update LoginButton.js

--- frontend/src/components/subcomponents/LoginButton.js
+++ frontend/src/components/subcomponents/LoginButton.js
@@ -8,10 +8,10 @@ const LoginButton = ({
     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     onClick={async () => {
       try {
-        // window.location.href = 'http://localhost:3000/auth/google'
-        await axios.get('/auth/google')
+        window.location.href = 'http://localhost:3000/auth/google'
+        // await axios.get('/auth/google')
         const navigate = switchScreens
-        navigate('/signup', { replace: true })
+        navigate('/gameboard', { replace: true })
       } catch (e) {
         alert('Incorrect Username/Password')
       }
